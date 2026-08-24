b16a3b24427da974b200273c59b943b669939353
rich-o1-blip <amosraut77@gmail.com>
2026-08-24 20:55:31 +0545

    traditional vs arrow function using javascript

---
+++ javascript/traditional-arrow-fn.js
@@ -0,0 +1,9 @@
+// Traditional Function
+function greet(user) {
+  return `Hello, ${user}!`;
+}
+
+// Arrow Function (Shorthand)
+const greetArrow = (user) => `Hello, ${user}!`;
+
+console.log(greetArrow("Bob")); // "Hello, Bob!"
